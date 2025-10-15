fcab8c0a18b3b48833171402b544874afd6558a2
Deep-Dey1 <deepdey715@gmail.com>
2025-10-15 23:29:02 +0530

    four routes created sucessfully

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,19 @@ const express = require("express")
 const app = express();
 
 app.get("/api/notes", (req,res) => {
-	res.send("First API now responsive using nodemon");
+	res.send("you got 20 notes");
+});
+
+app.post("/api/notes", (req,res) => {
+	res.status(201).json({message:"Notes created sucessfully!"});
+});
+
+app.put("/api/notes/:id", (req,res) => {
+	res.status(200).json({message:"Notes Updated Sucessfully!"});
+});
+
+app.delete("/api/notes/:id", (req,res) => {
+	res.status(200).json({message:"Notes Updated Sucessfully!"});
 });
 
 app.listen(5001, () =>{
