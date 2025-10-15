5bf906584d52a585b88c634b39fd774c73f5414c
Deep-Dey1 <deepdey715@gmail.com>
2025-10-16 00:02:09 +0530

    organise the structure a bit

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,11 @@
-//import express from "express";
-const express = require("express")
+import express from "express";
+//const express = require("express")
+import notesRoutes from "./routes/notesRoutes.js"
+// const notesRoutes = require("./routes/notesRoutes.js")
+
 const app = express();
 
-app.get("/api/notes", (req,res) => {
-	res.send("you got 20 notes");
-});
-
-app.post("/api/notes", (req,res) => {
-	res.status(201).json({message:"Notes created sucessfully!"});
-});
-
-app.put("/api/notes/:id", (req,res) => {
-	res.status(200).json({message:"Notes Updated Sucessfully!"});
-});
-
-app.delete("/api/notes/:id", (req,res) => {
-	res.status(200).json({message:"Notes Updated Sucessfully!"});
-});
+app.use("/api/notes", notesRoutes)
 
 app.listen(5001, () =>{
 	console.log("Server started on PORT: 5001");
