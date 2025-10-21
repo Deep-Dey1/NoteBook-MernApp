aafddc4fcc0d654ad461acb7989f7b69c0b59aa0
Deep-Dey1 <deepdey715@gmail.com>
2025-10-21 17:31:16 +0530

    console log added in the server using the middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,15 @@ connectDB();
 
 // middleware we needed
 // creating during the building of the create node controller
-app.use(express.json()); // this is the middle ware only
+app.use(express.json()); // this is middle ware
+
+// now genrating the console log in the backend server using the middle ware 
+app.use((req,res,next) =>{
+	console.log("Server got a New Request : ");
+	console.log(`Req methode is ${req.method} & Req URL is ${req.url}`);
+	next();
+});
+
 
 app.use("/api/notes", notesRoutes)
 
